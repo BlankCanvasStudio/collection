7dc665f7167800b443effafb82116a7ea4e76929
test <test@test.com>
2025-01-16 12:06:45 -0600

    bug fixes

--- install/byob.sh
+++ install/byob.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-set -xe
+set -e
 
 server_ip=""
 port=""
 modules=""
 location="/usr/local/bin"
+
+
+show_help() {
+    echo -e "Usage: ./install/byob.sh [flags] <C&C ip> <port> <modules>"
+    echo -e ""
+    echo -e "This command installs byob on the current xdc, builds a client binary, and uploads it to every node in the topology"
+    echo -e "\tThis program WON'T run byob on the target machine, you need to do that"
+    echo -e "\tYou can set the details of the client program via the cli. The output is a frozen binary, so there aren't any client side dependencies"
+    echo -e ""
+    echo -e "-h/--help shows this menu"
+    echo -e "-l sets the location to install the client binary"
+    echo -e "\tdefaults to /usr/local/bin/byob-client"
+    echo -e ""
+    echo -e "<xdc> is the xdc you'd like to install byob on"
+    echo -e "<C&C ip> is the ip the compromised machines should connect to to reach your command and control server"
+    echo -e "<port> is the open port on your C&C server"
+    echo -e "<modules> are the exploits / libraries you'd like to bundle in your target's binary"
+    echo -e ""
+}
 
 
 while [[ $# -gt 0 ]]; do
@@ -33,25 +52,6 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-
-
-show_help() {
-    echo -e "Usage: ./install/byob.sh [flags] <C&C ip> <port> <modules>"
-    echo -e ""
-    echo -e "This command installs byob on the current xdc, builds a client binary, and uploads it to every node in the topology"
-    echo -e "\tThis program WON'T run byob on the target machine, you need to do that"
-    echo -e "\tYou can set the details of the client program via the cli. The output is a frozen binary, so there aren't any client side dependencies"
-    echo -e ""
-    echo -e "-h/--help shows this menu"
-    echo -e "-l sets the location to install the client binary"
-    echo -e "\tdefaults to /usr/local/bin/byob-client"
-    echo -e ""
-    echo -e "<xdc> is the xdc you'd like to install byob on"
-    echo -e "<C&C ip> is the ip the compromised machines should connect to to reach your command and control server"
-    echo -e "<port> is the open port on your C&C server"
-    echo -e "<modules> are the exploits / libraries you'd like to bundle in your target's binary"
-    echo -e ""
-}
 
 
 
