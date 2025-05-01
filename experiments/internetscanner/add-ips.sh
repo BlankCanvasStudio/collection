c82cbd93e52accff02b8aa059f9f811aee50ec0d
test <test@test.com>
2025-05-01 14:07:54 -0500

    parallelism is grea

--- experiments/internetscanner/add-ips.sh
+++ experiments/internetscanner/add-ips.sh
@@ -27,12 +27,19 @@ index=0
 copy_index=0
 
 for ip in 107.125.{128..255}.{2..255}; do
-    version=$(($index % $total_nodes)) 
-    node_index=$(($index % $node_kinds)) 
+    (
+        version=$(($index % $total_nodes)) 
+        node_index=$(($index % $node_kinds)) 
 
-    ssh "${node_types[node_index]}$((copy_index + 1))" "sudo ip addr add $ip/17 dev eth1"
+        ssh "${node_types[node_index]}$((copy_index + 1))" "sudo ip addr add $ip/17 dev eth1"
+
+    ) &
 
     index=$(($index + 1))
     copy_index=$((($copy_index + 1) % $copies))
+
+    if [ "$(($index % 200))" = "0" ]; then
+        wait
+    fi
 done
 
