3b1e3124884ef566d07f60e99774a3dc9d812bdb
test <test@test.com>
2025-05-20 18:28:55 -0500

    version bump'

--- install/core.sh
+++ install/core.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-version="1.3.1"
+version="1.3.2"
 
 show_help() {
     echo "Usage: ./install-core.sh [flags] <node>"
--- install/sensors.sh
+++ install/sensors.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-version="1.3.1"
+version="1.3.2"
 
 show_help() {
     echo "Usage: ./install-sensors-xdc.sh [flags] [optional: nodes]"
